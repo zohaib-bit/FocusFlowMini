2a327b0be6b21bc533e646eb28a956680e5b7018
Zohaib <zohaibfaisalo9tech@gmail.com>
2025-11-11 14:50:32 +0500

    feat: again text change

--- FlowFocusMini/ContentView.swift
+++ FlowFocusMini/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hi, world!")
+            Text("How are you, world!")
         }
         .padding()
     }
