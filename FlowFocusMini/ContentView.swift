712523ce33fd8a95db37ee74c5ad3284b87238d9
Zohaib <zohaibfaisalo9tech@gmail.com>
2025-11-11 12:27:31 +0500

    Initial Commit

---
+++ FlowFocusMini/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlowFocusMini
+//
+//  Created by o9tech on 11/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlowFocusMini/FlowFocusMiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowFocusMiniApp.swift
+//  FlowFocusMini
+//
+//  Created by o9tech on 11/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlowFocusMiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
